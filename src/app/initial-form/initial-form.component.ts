83fe250d1308d5b467f6a66336f30b4e1208b379
KrashMunoz <krash.munoz@gmail.com>
2023-12-18 01:14:42 -0500

    Added autocomplete to departure and destination location forms; included debouncetime and distinct untilchanged

--- src/app/initial-form/initial-form.component.ts
+++ src/app/initial-form/initial-form.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -6,7 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { Subscription, first } from 'rxjs';
+import { Observable, Subscription, debounceTime, distinctUntilChanged, first, map, startWith, tap } from 'rxjs';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import * as citiesJSON from '../../assets/data/cities.json';
@@ -23,52 +23,62 @@ import * as citiesJSON from '../../assets/data/cities.json';
     MatAutocompleteModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    AsyncPipe
   ],
   templateUrl: './initial-form.component.html',
   styleUrl: './initial-form.component.scss'
 })
 
 export class InitialFormComponent implements OnInit, OnDestroy {
-  @ViewChild('input') input!: ElementRef<HTMLInputElement>;
+  // formChanges: Subscription;
+
   intialForm: FormGroup = new FormGroup({
     tripTypeControl: new FormControl([], Validators.required),
-    departureLocation: new FormControl([], Validators.required),
+    departureLocation: new FormControl<string>('', Validators.required),
     destinationLocation: new FormControl([], Validators.required),
     departureDate: new FormControl<Date | null>(null),
     dateRange: new FormGroup({
       start: new FormControl<Date | null>(null),
       end: new FormControl<Date | null>(null)
     }),
-    passengerControl: new FormControl([], Validators.required)
+    passengerControl: new FormControl([], [Validators.required, Validators.min(1), Validators.max(9)])
   })
-
-  formChanges: Subscription;
 
   tripOptions = [
     { description: 'One-way', value: 'one-way' },
     { description: 'Round-trip', value: 'round-trip' }
   ];
 
-  cityOptions: Array<string> = citiesJSON.cities;
-  filteredCityOptions: Array<string>;
+  cityOptions: string[] = citiesJSON.cities;
+  filteredDepartureCities: Observable<string[]> | undefined;
+  filteredDestinationCities: Observable<string[]> | undefined;
 
   constructor() {
-    this.formChanges = this.intialForm.valueChanges.pipe()
-      .subscribe(formVal => console.log(formVal));
-
-    this.filteredCityOptions = this.cityOptions.slice()
+    // this.formChanges = this.intialForm.valueChanges.pipe()
+    //   .subscribe(formVal => console.log(formVal));
   }
 
-  filter() {
-    const filterValue = this.input.nativeElement.value.toLowerCase();
-    this.filteredCityOptions = this.cityOptions.filter(o => o.toLowerCase().includes(filterValue));
+  private _filter(value: string): string[] {
+    const filterValue = value?.toLowerCase();
+    return this.cityOptions.filter(option => option?.toLowerCase().includes(filterValue))
   }
 
   ngOnInit(): void {
-    console.log("KM InitialComponent cityOptions =", this.cityOptions);
+    this.filteredDepartureCities = this.intialForm.get('departureLocation')?.valueChanges.pipe(
+      startWith(''),
+      debounceTime(250),
+      distinctUntilChanged(),
+      map(value => this._filter(value))
+    )
+    this.filteredDestinationCities = this.intialForm.get('destinationLocation')?.valueChanges.pipe(
+      startWith(''),
+      debounceTime(250),
+      distinctUntilChanged(),
+      map(value => this._filter(value))
+    )
   }
 
   ngOnDestroy(): void {
-    this.formChanges.unsubscribe();
+    // this.formChanges.unsubscribe();
   }
 }
